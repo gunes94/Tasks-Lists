9014a4dc462c6ad477c95b6e3648b15956dc621c
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:52:45 +0200

    Create Flag.java

---
+++ Flag.java
@@ -0,0 +1,9 @@
+mport java.util.Arrays;
+
+public class Flag {
+    public static void main(String[] args) {
+        char[] arr = {};
+        Arrays.fill(arr, '\u2588');
+        System.out.println(arr);
+    }
+}
